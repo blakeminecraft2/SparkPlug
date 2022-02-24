b521af00ca712799e8cc3957744ff4397da3e99a
blakeminecraft2 <100368620+blakeminecraft2@users.noreply.github.com>
2022-02-24 11:09:27 -0500

    Update secret.py

--- cogs/commands/secret.py
+++ cogs/commands/secret.py
@@ -7,7 +7,7 @@ class Secret(commands.Cog):
         self.emoji = "🐻‍❄️"
 
     @commands.command(slash_command=False)
-    async def pet(self,ctx: commands.Context,user: discord.Member):
+    async def Secret(self,ctx: commands.Context,user: discord.Member):
         msg = await ctx.send(f"You walk up to *{user.name}* and fuck them in the ass so hard it comes out their nose and mouth")
         await msg.add_reaction("😻")
 
