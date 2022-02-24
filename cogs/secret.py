d33eaa129dd66463ec23de972f5794db74efd24d
blakeminecraft2 <100368620+blakeminecraft2@users.noreply.github.com>
2022-02-24 11:08:05 -0500

    Create secret.py

---
+++ cogs/secret.py
@@ -0,0 +1,15 @@
+import discord
+from discord.ext import commands
+
+class Secret(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.emoji = "🐻‍❄️"
+
+    @commands.command(slash_command=False)
+    async def pet(self,ctx: commands.Context,user: discord.Member):
+        msg = await ctx.send(f"You walk up to *{user.name}* and fuck them in the ass so hard it comes out their nose and mouth")
+        await msg.add_reaction("😻")
+
+def setup(bot):
+    bot.add_cog(Secret(bot))
